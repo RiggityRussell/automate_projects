321a9bf6a2b6568318112a1afd4f97752955df65
RiggityRussell <arlt9@mail.nmc.edu>
2022-05-02 19:34:49 -0400

    1st automate assignment

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+#repetative practice to solidify my skills
+
+print('Hello World')
+user_name=input('what is your name?')
+print(f'It is nice to meet you {user_name}')
+print(f'The length of your name is {len(user_name)}')
+user_age=input('what is your age?')
+print(f'you will be {str(int(user_age) +1)} in a year') #this is a useful thing manipulating the int and string of the variable.
